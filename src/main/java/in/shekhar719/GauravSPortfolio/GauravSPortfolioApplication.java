ce1537ee8af0566257595d5f3b4b57de33424ebb
Gaurav <gaurav.shekhar7197@gmail.com>
2025-06-22 18:40:33 +0530

    First commit

---
+++ src/main/java/in/shekhar719/GauravSPortfolio/GauravSPortfolioApplication.java
@@ -0,0 +1,13 @@
+package in.shekhar719.GauravSPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GauravSPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GauravSPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/shekhar719/GauravSPortfolio/GauravSPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package in.shekhar719.GauravSPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GauravSPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
